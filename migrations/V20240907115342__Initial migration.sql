d914d6ed32893409be5e08650a5b76cc233b851d
Fábio Costa <fabioatc@gmail.com>
2024-09-11 00:27:20 +0100

    Setup project for ES6, flyway and initial migration

--- migrations/V20240907115342__Initial migration.sql
+++ migrations/V20240907115342__Initial migration.sql
@@ -1,5 +1,7 @@
 -- Write your migration SQL here
 CREATE TABLE IF NOT EXISTS users (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  login VARCHAR(255) UNIQUE NOT NULL,
+  name VARCHAR(255),
   location VARCHAR(255)
 );
